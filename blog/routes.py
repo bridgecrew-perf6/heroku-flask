7bfd8e618c87890c574f7f77e3bbaece0b279385
Francesco Schena <francesco@schena.eu>
2020-12-24 00:10:46 +0100

    ''

--- blog/routes.py
+++ blog/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template, url_for, flash, redirect, request, send_from_directory
 from blog.forms import RegistrationForm, LoginForm
 from blog.models import User, Post
 from blog import app, bcrypt, db
@@ -27,7 +28,7 @@ def home():
     return render_template("home.html", posts=postsdic)
 
 
-@app.route("/favicon.ico")
+@app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
