cd60477d608821cff2b000aa0356b166a20880a9
Francesco Schena <francesco@schena.eu>
2020-12-26 19:12:16 +0100

    Added gzip compression

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,16 +1,23 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-
+from flask_compress import Compress
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = '2e621fcaf9cd4f0d073ecdf123314ba9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+Compress(app)
+
+# don't move this import
 from blog import routes
